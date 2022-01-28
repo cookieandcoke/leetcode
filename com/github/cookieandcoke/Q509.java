289736921fc898f47066942e0d973c71fdb8135c
cookieandcoke <6669587+cookieandcoke@users.noreply.github.com>
2022-01-28 14:46:31 +0800

    add several solutions

--- com/github/cookieandcoke/Q509.java
+++ com/github/cookieandcoke/Q509.java
@@ -5,7 +5,16 @@ package com.github.cookieandcoke;
  * https://leetcode.com/problems/fibonacci-number/
  */
 public class Q509 {
-    public int fib(int n) {
+
+    public int fib_rec(int n) {
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        return fib_rec(n-1) + fib_rec(n-2);
+    }
+
+    public int fib_var(int n) {
         if(n==0)
             return 0;
         if(n==1)
@@ -22,5 +31,13 @@ public class Q509 {
             b=r;
         }
         return r;
+    }
+
+    public int fib_array(int n) {
+        int[] arr=new int[31];
+        arr[0]=0; arr[1]=1;
+        for(int i=2; i<arr.length; i++)
+            arr[i]=arr[i-1]+arr[i-2];
+        return arr[n];
     }
 }
